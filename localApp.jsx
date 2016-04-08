50233b6041d00f131525bd1de8e457f50e24183e
Dennis Snell <dennis.snell@automattic.com>
2016-04-08 01:03:27 +0100

    Compare side-by-side with and without localStorage cache

--- localApp.jsx
+++ localApp.jsx
@@ -2,20 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
+import persistState from 'redux-localstorage';
+import { fromJSON, toJSON } from 'transit-immutable-js';
 
 import { AppFactory } from 'src/app';
 import RouteWrapper from 'routing-wrapper';
 import OAuthApp from 'oauth-wrapper';
 
-const RoutedApp = RouteWrapper( AppFactory, applyMiddleware( createLogger() ) );
+const containerStyle = {
+	width: '50%',
+	height: '100%',
+	float: 'left',
+	position: 'relative'
+}
+
+const RoutedApp = props => (
+	<div style={ containerStyle }>
+		{ RouteWrapper( AppFactory, applyMiddleware( createLogger() ) )( props ) }
+	</div>
+);
+
+const statePersister = persistState( null, {
+	serialize: toJSON,
+	deserialize: fromJSON
+} );
+
+const CachedApp = props => (
+	<div style={ containerStyle }>
+		{ React.createElement( AppFactory( statePersister ).App, props ) }
+	</div>
+);
 
 const clientId = 35604;
 const redirectPath = '/connect/response';
 
+const Tee = givenProps => {
+	const { children, ...props } = givenProps;
+
+	return (
+		<div>
+			{ React.Children.map( children, Child => React.cloneElement( Child, props ) ) }
+		</div>
+	);
+}
+
 ReactDOM.render(
 	<OAuthApp { ...{ clientId, redirectPath } }>
-		<RoutedApp />
+		<Tee>
+			<RoutedApp />
+			<CachedApp />
+		</Tee>
 	</OAuthApp>,
 	document.getElementById( 'root' )
 );
-
